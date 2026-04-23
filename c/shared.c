ac0a42fa5b0ffba229a2a8469585f1a0d1c7ecd6
Kacper Michalik <km924@cam.ac.uk>
2026-04-23 02:28:54 +0100

    Added performance profiling version of solver

--- c/shared.c
+++ c/shared.c
@@ -9,12 +9,6 @@
 #include "init.h"
 #include "simulation.h"
 
-/*
- char *outname;
-    int output = 0;
-    int output_frequency = 0;
-*/
-
 int simulate(int verbose, char* outname, int output, int output_frequency, double xlength, double ylength, int imax, int jmax, double t_end, double del_t, double tau, 
 int itermax, double eps, double omega, double gamma, double Re, double ui, double vi, 
 double** u, double** v, double** p, double** rhs, double** f, double** g, char** flag)
@@ -82,6 +76,46 @@ double** u, double** v, double** p, double** rhs, double** f, double** g, char**
             write_ppm(u, v, p, flag, imax, jmax, xlength, ylength, outname,
                 iters, output_frequency);
         }
+    }
+
+    return 0;
+}
+
+
+int simulate_profiling(int init_ibound, double xlength, double ylength, int imax, int jmax, double t_end, double del_t, double tau, 
+int itermax, double eps, double omega, double gamma, double Re, double ui, double vi, 
+double** u, double** v, double** p, double** rhs, double** f, double** g, char** flag)
+{
+    double t, delx, dely;
+    int  i, j, itersor = 0, ifluid = 0, ibound = init_ibound;
+    double res;
+    int iters = 0;
+
+    delx = xlength/imax;
+    dely = ylength/jmax;
+    
+    // Main loop
+    apply_boundary_conditions(u, v, flag, imax, jmax, ui, vi);
+    for (t = 0.0; t < t_end; t += del_t, iters++) {
+        set_timestep_interval(&del_t, imax, jmax, delx, dely, u, v, Re, tau);
+
+        ifluid = (imax * jmax) - ibound;
+
+        compute_tentative_velocity(u, v, f, g, flag, imax, jmax,
+            del_t, delx, dely, gamma, Re);
+
+        compute_rhs(f, g, rhs, flag, imax, jmax, del_t, delx, dely);
+
+        if (ifluid > 0) {
+            itersor = poisson(p, rhs, flag, imax, jmax, delx, dely,
+                        eps, itermax, omega, &res, ifluid);
+        } else {
+            itersor = 0;
+        }
+	
+        update_velocity(u, v, f, g, p, flag, imax, jmax, del_t, delx, dely);
+
+        apply_boundary_conditions(u, v, flag, imax, jmax, ui, vi);
     }
 
     return 0;
